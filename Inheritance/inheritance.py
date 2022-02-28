ba4fb8dd420d38792e025b53fe8abb73cc119b72
Darcy Merilan <darcymer@byui.edu>
2022-02-28 13:58:50 -0700

    stuff

---
+++ Inheritance/inheritance.py
@@ -0,0 +1,15 @@
+
+class person:
+
+    def get_name(self):
+        return "Joshua"
+
+class student(person):
+    def get_number(self):
+        return "0123456789"
+
+"""jane = student()
+print(jane.get_name())
+print(jane.get_number())"""
+
+
---
+++ Inheritance/phonechallenge.py
@@ -0,0 +1,34 @@
+class phone:
+    def  __init__(self):
+        self._phone_number = 0
+        self._text_message = []
+        pass
+    
+    def place_call(number_to_call):
+        pass
+
+    def place_text(number_to_text, message_to_send):
+        pass
+
+    def save_text(message_to_save):
+        pass
+
+    def get_texts():
+        pass
+
+    def get_number():
+        pass
+
+    pass
+    
+    def get_phone_num(self):
+        pass self.phone_number
+
+class CameraPhone(phone):
+   def __init__(self):
+       super().__init__()
+       self.pictures = []
+    
+    def take_pictures(picture_name)
+        pass 
+    #pictures.append =[]
